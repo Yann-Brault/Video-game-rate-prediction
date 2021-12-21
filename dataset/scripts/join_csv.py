4a010a96e74a97af78b8be075bb675d039e2eee7
Antoine Vidal-Mazuy <antoine.vidalma@gmail.com>
2021-12-21 15:56:30 +0100

    dataset 2.0

---
+++ dataset/scripts/join_csv.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from pandas.core.frame import DataFrame
+from tqdm import trange
+
+
+jeux = []
+note = []
+cbm = []
+neg = []
+pos = []
+avis = []
+
+acc = 0
+for i in range(103):
+    print(i)
+    try:
+        data_tmp = pd.read_csv(f'ant-csv/data_part_{i}.csv')
+
+        for j in trange(data_tmp.shape[0]):
+            jeux.append(data_tmp['jeux'][j])
+            note.append(data_tmp['note'][j])
+            cbm.append(data_tmp['classe_bon_mauvais'][j])
+            neg.append(data_tmp['negative_words'][j])
+            pos.append(data_tmp['positive_words'][j])
+            avis.append(data_tmp['avis'][j])
+            
+            # data.iloc[acc, 'jeux'] = jeux
+            # data.iloc[acc, 'note'] = note
+            # data.iloc[acc, 'classe_bon_mauvais'] = cbm
+            # data.iloc[acc, 'negative_words'] = neg
+            # data.iloc[acc, 'positive_words'] = pos
+            # data.iloc[acc, 'avis'] = avis
+            # acc+=1
+
+    except Exception as e:
+        print(i, e)
+
+data = pd.DataFrame(data={'jeux': jeux, 'note': note, 'classe_bon_mauvais': cbm, 'negative_words': neg, 'positive_words': pos, 'avis': avis})
+data.to_csv('dataset/csv/dataset_clean_2.0.csv')
+
